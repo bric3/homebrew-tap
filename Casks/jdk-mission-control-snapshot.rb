c17fd3531653d3d9c50dd4f3e7706a499e895e64
Brice Dutheil <brice.dutheil@gmail.com>
2021-09-09 21:30:51 +0200

    Updates JMC snapshot link to 8.2.0

--- Casks/jdk-mission-control-snapshot.rb
+++ Casks/jdk-mission-control-snapshot.rb
@@ -2,7 +2,7 @@ cask "jdk-mission-control-snapshot" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/AdoptOpenJDK/openjdk-jmc-overrides/releases/download/8.1.0-SNAPSHOT/org.openjdk.jmc-8.1.0-SNAPSHOT-macosx.cocoa.x86_64.tar.gz'
+  url 'https://github.com/adoptium/jmc-overrides/releases/download/8.2.0-SNAPSHOT/org.openjdk.jmc-8.2.0-SNAPSHOT-macosx.cocoa.x86_64.tar.gz'
   name "JDK Mission Control"
   desc "Tools to manage, monitor, profile and troubleshoot Java applications"
   homepage 'https://adoptopenjdk.net/jmc.html'
