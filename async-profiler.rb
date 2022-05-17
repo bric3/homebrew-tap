85bedaa404890b32c289f594c96b93295e72a462
Brice Dutheil <brice.dutheil@gmail.com>
2022-05-17 15:45:40 +0200

    async-profiler 2.8 macOs

--- async-profiler.rb
+++ async-profiler.rb
@@ -1,11 +1,11 @@
 class AsyncProfiler < Formula
   desc "Sampling CPU and HEAP profiler for Java featuring AsyncGetCallTrace + perf_events"
   homepage "https://github.com/jvm-profiling-tools/async-profiler"
-  url "https://github.com/jvm-profiling-tools/async-profiler/releases/download/v2.5.1/async-profiler-2.5.1-macos.zip"
-  sha256 "ebd7409f15fa579e91fb1ef1263d285e526b2e87c3f0354975c2477889db6f02"
+  url "https://github.com/jvm-profiling-tools/async-profiler/releases/download/v2.8/async-profiler-2.8-macos.zip"
+  sha256 "d5bf9758f479e7873e81c61c9cff376bd82202aa56028f851f497b508f177b1e"
   license "Apache-2.0"
 
-  # Fix script location
+  # Fix script location (make the diff with 'diff -u orignal/profiler.sh pathed/profiler.sh')
   patch :p0, :DATA
 
   def install
@@ -18,19 +18,27 @@ class AsyncProfiler < Formula
 
   # test do
   #   output = shell_output("#{bin}/profiler.sh --version")
-  #   assert_match(/Async-profiler 2.5.1.+/, output.strip)
+  #   assert_match(/Async-profiler 2.8.+/, output.strip)
   # end
 end
 
 __END__
---- profiler.sh	2021-12-07 01:10:43.000000000 +0100
-+++ profiler.sh	2022-01-04 10:51:04.000000000 +0100
-@@ -124,7 +124,7 @@
+--- profiler.sh	2022-05-09 21:59:25.000000000 +0200
++++ profiler.sh	2022-05-17 15:31:21.000000000 +0200
+@@ -125,12 +125,12 @@
  while [ -h "$SCRIPT_BIN" ]; do
      SCRIPT_BIN="$(readlink "$SCRIPT_BIN")"
  done
 -SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_BIN")" > /dev/null 2>&1; pwd -P)"
 +SCRIPT_DIR="$(cd -- "$(dirname "$0")"; cd -- "$(dirname "$SCRIPT_BIN")" > /dev/null 2>&1; pwd -P)"
-
- JATTACH=$SCRIPT_DIR/build/jattach
- FDTRANSFER=$SCRIPT_DIR/build/fdtransfer
+ 
+-JATTACH=$SCRIPT_DIR/build/jattach
+-FDTRANSFER=$SCRIPT_DIR/build/fdtransfer
++JATTACH=$SCRIPT_DIR/../libexec/build/jattach
++FDTRANSFER=$SCRIPT_DIR/../libexec//build/fdtransfer
+ USE_FDTRANSFER="false"
+-PROFILER=$SCRIPT_DIR/build/libasyncProfiler.so
++PROFILER=$SCRIPT_DIR/../libexec/build/libasyncProfiler.so
+ ACTION="collect"
+ DURATION="60"
+ FILE=""
