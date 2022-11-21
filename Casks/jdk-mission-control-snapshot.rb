9529a6e617e12f15253104da64f492d732477b8d
Brice Dutheil <brice.dutheil@gmail.com>
2022-11-21 11:55:45 +0100

    chore: Support different arch for JDK Mission Control

--- Casks/jdk-mission-control-snapshot.rb
+++ Casks/jdk-mission-control-snapshot.rb
@@ -2,8 +2,17 @@ cask "jdk-mission-control-snapshot" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/adoptium/jmc-build/releases/download/9.0.0-SNAPSHOT/org.openjdk.jmc-9.0.0-SNAPSHOT-macosx.cocoa.x86_64.tar.gz'
-  name "JDK Mission Control"
+  if OS.linux?
+    # Only Intel is supported
+    url "https://github.com/adoptium/jmc-build/releases/download/9.0.0-SNAPSHOT/org.openjdk.jmc-9.0.0-SNAPSHOT-linux.gtk.x86_64.tar.gz"
+  else
+    if Hardware::CPU.intel?
+      url "https://github.com/adoptium/jmc-build/releases/download/9.0.0-SNAPSHOT/org.openjdk.jmc-9.0.0-SNAPSHOT-macosx.cocoa.x86_64.tar.gz"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/adoptium/jmc-build/releases/download/9.0.0-SNAPSHOT/org.openjdk.jmc-9.0.0-SNAPSHOT-macosx.cocoa.aarch64.tar.gz"
+    end 
+  end
+    name "JDK Mission Control"
   desc "Tools to manage, monitor, profile and troubleshoot Java applications"
   homepage 'https://adoptopenjdk.net/jmc.html'
 
