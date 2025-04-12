f8a22cfd8f65fd365ef63f0b4b872d9bbbd2948f
Brice Dutheil <brice.dutheil@gmail.com>
2025-04-13 01:38:29 +0200

    chore: async-profiler to 4.0
    Async profiler is now a binary since v3.0
    
    https://github.com/async-profiler/async-profiler/releases/tag/v4.0

--- async-profiler.rb
+++ async-profiler.rb
@@ -4,76 +4,42 @@ class AsyncProfiler < Formula
   license "Apache-2.0"
   if OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/jvm-profiling-tools/async-profiler/releases/download/v2.9/async-profiler-2.9-linux-x64.tar.gz"
-      sha256 "b9a094bc480f233f72141b7793c098800054438e0e6cfe5b7f2fe13ef4ad11f0"
+      url "https://github.com/async-profiler/async-profiler/releases/download/v4.0/async-profiler-4.0-linux-x64.tar.gz"
+      sha256 "ba32bd69cc2906f6251cd5aa8ae72c3abc12267ba858d38733f7751cc4d3e05b"
     elsif Hardware::CPU.arm?
-      url "https://github.com/jvm-profiling-tools/async-profiler/releases/download/v2.9/async-profiler-2.9-linux-arm64.tar.gz"
-      sha256 "00c91439b2fc95fa518e47570c451d92c8670c0cd68d908ee53397bb35a42779"
+      url "https://github.com/async-profiler/async-profiler/releases/download/v4.0/async-profiler-4.0-linux-arm64.tar.gz"
+      sha256 "482c142075dd8eeb762627087dd663725631e2747822f4480753e3b5507b3a62"
     end
   else
     # The macOs distribution works for intel and arm64
-    url "https://github.com/jvm-profiling-tools/async-profiler/releases/download/v2.9/async-profiler-2.9-macos.zip"
-    sha256 "7f46398aef41561e77012ecbd07d3cc17eac775c03e895df4fcc174f8af2ecc2"
+    url "https://github.com/async-profiler/async-profiler/releases/download/v4.0/async-profiler-4.0-macos.zip"
+    sha256 "1f27e5f5952ec40126a22addad6989bcbeb0509da8a2714ec489953abd698360"
   end
-
-  # don't download openjdk, it may have been installed by other means 
-  # depends_on "openjdk"
-
-  # Fix script location (make the diff with 'diff -u orignal/profiler.sh pathed/profiler.sh')
-  patch :p0, :DATA
 
   def install
-    # bin.install Dir["*"]
+    ohai "Installing #{HOMEBREW_PREFIX}/bin/asprof"
+    bin.install Dir["bin/*"]
+    ohai "Installing #{HOMEBREW_PREFIX}/lib/#{shared_library("libasyncProfiler")}"
+    lib.install Dir["lib/*"]
 
-    libexec.install Dir["*"]
-    # libexec.install_symlink "#{libexec}/profiler.sh" => "profiler.sh"
-    bin.install_symlink "#{libexec}/profiler.sh"
+    # Neet to codesign the lib ?
+    # codesign -v -vvv /opt/homebrew/lib/libasyncProfiler.dylib => not signed
+    # libasyncProfiler = shared_library("libasyncProfiler")
+    # MachO.codesign!(lib/libasyncProfiler)
   end
 
-  def post_install
-    if OS.mac?
-      # Using the .so extension for the library file because async-profiler builds
-      # the macOs library with the .so extension, hence the dylib symlink in the same directory
-      Dir["#{libexec}/build/*.so"].each do |dylib|
-        chmod 0664, dylib
-        # id found with otool -L async-profiler-2.8-macos/build/libasyncProfiler.so
-        MachO::Tools.change_dylib_id(dylib, "build/#{File.basename(dylib)}")
-        chmod 0444, dylib
-      end
-    end
-  end
-
-  # Tests disabled because the test environment doesn't find java
-  # test do
-  #   output = shell_output("#{bin}/profiler.sh --version")
-  #   assert_match(/Async-profiler 2.8.+/, output.strip)
+  # def post_install
+  #   libasyncProfiler = "#{HOMEBREW_PREFIX}/lib/#{shared_library("libasyncProfiler")}"
+  #   ohai <<~EOS
+  #   Usage:
+  #     Adhoc: asprof start -d 10s -o collapsed,flamegraph,svg,flamegraph.svg
+  #     JVM agent: -agentpath:#{libasyncProfiler}=start,event=cpu,alloc=2m,lock=10ms,file=profile.jfr
+  #     Native: LD_PRELOAD=#{libasyncProfiler} ASPROF_COMMAND=start,event=cpu,file=profile.jfr NativeApp [args]
+  #   EOS
   # end
 
-  def caveats
-    <<~EOS
-      To use async-profiler, you need to install a JVM.
-    EOS
+  test do
+    output = shell_output("#{bin}/asprof --version")
+    assert_match(/Async-profiler 4.0.+/, output.strip)
   end
 end
-
-__END__
---- profiler 1.sh	2022-11-27 15:11:24
-+++ profiler.sh	2022-11-27 15:13:37
-@@ -130,13 +130,13 @@
- while [ -h "$SCRIPT_BIN" ]; do
-     SCRIPT_BIN="$(readlink "$SCRIPT_BIN")"
- done
--SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_BIN")" > /dev/null 2>&1; pwd -P)"
-+SCRIPT_DIR="$(cd -- "$(dirname "$0")"; cd -- "$(dirname "$SCRIPT_BIN")" > /dev/null 2>&1; pwd -P)"
-
--JATTACH=$SCRIPT_DIR/build/jattach
--FDTRANSFER=$SCRIPT_DIR/build/fdtransfer
-+JATTACH=$SCRIPT_DIR/../libexec/build/jattach
-+FDTRANSFER=$SCRIPT_DIR/../libexec/build/fdtransfer
- USE_FDTRANSFER="false"
- FDTRANSFER_PATH=""
--PROFILER=$SCRIPT_DIR/build/libasyncProfiler.so
-+PROFILER=$SCRIPT_DIR/../libexec/build/libasyncProfiler.so
- ACTION="collect"
- DURATION="60"
- FILE=""
